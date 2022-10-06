cad2e026b49018643331950f48332a9a06a304e3
guyluz11 <guyluz11@gmail.com>
2022-10-06 14:20:50 +0300

    Fixed snap use wrong project location + fixed project folder name. Updated dart version to 2.18.2

--- scripts/bashScripts/nativeExecutableMaker.sh
+++ scripts/bashScripts/nativeExecutableMaker.sh
@@ -7,13 +7,13 @@ scripts/bashScripts/dartSdkDownload.sh # Downloading dart-sdk for the correct ar
 unzip dartsdk-*.zip
 rm dartsdk-*.zip
 
-cd SmartDeviceDart
+cd cbj_smart-device
 ../dart-sdk/bin/pub get --no-precompile
 #sudo ../dart-sdk/bin/pub run build_runner build --delete-conflicting-outputs
 cd ..
 
 #chmod -R +rx dart-sdk/
-dart-sdk/bin/dart2native SmartDeviceDart/bin/main.dart -o Cbj-Smart-Device
-# sudo dart-sdk/bin/dart SmartDeviceDart/bin/main.dart .
+dart-sdk/bin/dart2native cbj_smart-device/bin/main.dart -o Cbj-Smart-Device
+# sudo dart-sdk/bin/dart cbj_smart-device/bin/main.dart .
 
 rm -r dart-sdk/
